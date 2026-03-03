04c669f2a513d4858d5a6f9c61981a6a5f6b273d
OptiFox <80470592+OptiFox@users.noreply.github.com>
2026-03-04 06:41:59 +0800

    Add User Interface (UI) to the Rock Paper Scissors game
    Refactored the code to add functionality to the UI

--- script.js
+++ script.js
@@ -4,12 +4,8 @@ function getComputerChoice() {
     return (random <= 33) ? 'rock' : (random <= 66) ? 'paper' : 'scissors';
 }
 
-function getHumanChoice() {
-    return prompt("Enter your choice (rock/paper/scissors): ");
-}
-
 let humanScore = 0;
-let computerScore = 0; 
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
@@ -18,54 +14,86 @@ function playRound(humanChoice, computerChoice) {
     const capitalizeHuman = humanChoice[0].toUpperCase() + humanChoice.slice(1);
     const capitalizeComputer = computerChoice[0].toUpperCase() + computerChoice.slice(1);
 
-    let outputTie = () => console.log("It's a tie!");
     let outputWin = () => {
-        console.log(`You won! ${capitalizeHuman} beats ${capitalizeComputer}.`);
         humanScore++;
+        return `You won! ${capitalizeHuman} beats ${capitalizeComputer}.`;
     };
     let outputLose = () => {
-        console.log(`You lose! ${capitalizeComputer} beats ${capitalizeHuman}.`);
         computerScore++;
+        return `You lose! ${capitalizeComputer} beats ${capitalizeHuman}.`;
     };
 
     if (humanChoice === computerChoice) {
-        outputTie();
+        return "It's a tie!";
     }
 
     if (humanChoice === 'rock') {
         if (computerChoice === 'paper') {
-            outputLose();
+            return outputLose();
         } else if (computerChoice === 'scissors') {
-            outputWin();
+            return outputWin();
         }
     } else if (humanChoice === 'paper') {
         if (computerChoice === 'rock') {
-            outputWin();
+            return outputWin();
         } else if (computerChoice === 'scissors') {
-            outputLose();
+            return outputLose();
         }
     } else if (humanChoice === 'scissors') {
         if (computerChoice === 'rock') {
-            outputLose();
+            return outputLose();
         } else if (computerChoice === 'paper') {
-            outputWin();
+            return outputWin();
         }
     }
 }
 
-function playGame() {
-    const MAX_ROUND = 5;
+const buttons = document.querySelectorAll(".btn button")
+let humanSelection = "";
 
-    for (let rounds = 1; rounds <= MAX_ROUND; rounds++) {
-        let humanSelection = getHumanChoice();
+const displayScore = document.querySelector(".score");
+const displayHuman = document.querySelector(".human");
+const displayComputer = document.querySelector(".computer");
+
+const displayResult = document.querySelector(".result");
+const result = document.createElement("p");
+
+displayHuman.textContent = "Human Score: " + humanScore;
+displayComputer.textContent = "Computer Score: " + computerScore;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        humanSelection = button.textContent;
         let computerSelection = getComputerChoice();
 
-        console.log(`\nRound #${rounds}\n`);
-        playRound(humanSelection, computerSelection);
+        if (humanSelection !== "") {
+            result.textContent = playRound(humanSelection, computerSelection);
+        }
 
-        console.log("\nHuman Score: " + humanScore);
-        console.log("Computer Score: " + computerScore);
-    }
-}
+        displayHuman.textContent = "Human Score: " + humanScore;
+        displayComputer.textContent = "Computer Score: " + computerScore;
 
-playGame();
+        if (humanScore >= 5) {
+            result.textContent = "Game ended! You won!";
+            for (const button of buttons) button.disabled = true;
+        } else if (computerScore >= 5) {
+            result.textContent = "Game ended! Computer won!";
+            for (const button of buttons) button.disabled = true;
+        }
+    });
+})
+
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click", () => {
+    for (const button of buttons) button.disabled = false;
+    humanScore = 0;
+    computerScore = 0;
+
+    displayHuman.textContent = "Human Score: " + humanScore;
+    displayComputer.textContent = "Computer Score: " + computerScore;
+})
+
+displayScore.appendChild(displayHuman);
+displayScore.appendChild(displayComputer);
+
+displayResult.appendChild(result);
